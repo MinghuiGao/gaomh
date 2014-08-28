f501e526d087486e0aff6c5f3b01a7f5b003cdae
gaomh <ham_777@126.com>
2014-08-28 17:58:05 +0800

    添加了一个组合和继承相关的性能测试

---
+++ DesignPattern/src/cn/gaomh/compositionNextends/Son.java
@@ -0,0 +1,17 @@
+package cn.gaomh.compositionNextends;
+
+
+public class Son extends Father {
+
+	public Son(String name) {
+		super(name);
+	}
+
+	public void run(){
+		super.run();
+	}
+	
+	public void run2(){
+		super.run2();
+	}
+}
