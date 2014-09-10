253cda620f2a4fdae889bea3c03f8c93c44301b1
gaomh <ham_777@126.com>
2014-09-10 17:34:50 +0800

    add jni

---
+++ DesignPattern/src/cn/gaomh/taskqueue/package-info.java
@@ -0,0 +1,24 @@
+/**
+ * @包名称 cn.gaomh.taskqueue
+ * @文件名 package-info.java
+ * @版本 1.0
+ * @创建者 gaomh
+ * @创建时间 Sep 10, 2014 3:03:22 PM
+ * @版权所有 ©2014 CTFO
+ * @修改者 gaomh
+ * @修改时间 Sep 10, 2014 3:03:22 PM
+ * @修改描述 
+ */
+
+/** 
+ * 功能描述 
+ * @类型名称 package-info
+ * @版本 1.0
+ * @创建者 gaomh
+ * @创建时间 Sep 10, 2014 3:03:22 PM
+ * @版权所有 ©2014 CTFO
+ * @修改者 gaomh
+ * @修改时间 Sep 10, 2014 3:03:22 PM
+ * @修改描述 
+ */
+package cn.gaomh.taskqueue;
